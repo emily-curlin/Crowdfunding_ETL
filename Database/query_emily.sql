ea43869d22f17faef06427a8afc4e96a2b592699
emily-curlin <125771374+emily-curlin@users.noreply.github.com>
2023-06-07 22:39:07 -0400

    initial file setup

---
+++ Database/query_emily.sql
@@ -0,0 +1,15 @@
+-- query the database to ensure tables work
+SELECT * 
+FROM category
+
+SELECT * 
+FROM subcategory
+
+SELECT * 
+FROM crowdfunding
+
+SELECT * 
+FROM campaign
+
+SELECT * 
+FROM contacts
